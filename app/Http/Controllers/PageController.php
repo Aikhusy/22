0a5deac885effa05d913af75d5c0e716f970450a
Syaikhul Shafwan R <heymepoo@gmail.com>
2023-03-02 11:57:32 +0700

    seccond

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -7,4 +7,16 @@ use Illuminate\Http\Request;
 class PageController extends Controller
 {
     //
+    public function index(){
+        return 'Selamat Datang';
+    }
+
+    public function about(){
+        return 'Syaikhul Shafwan R/2141720105';
+    }
+
+    public function articles($id){
+        return 'Article ke '. $id;
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
-
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/about', [AboutController::class, 'index']);
+
+Route::get('/article/{id}', [ArticleController::class, 'index']);
